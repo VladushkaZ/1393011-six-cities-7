9c9b99376cb2c633277dcf205deb2ba971dae266
Vlada Zorina <zorv@gkomega.ru>
2021-06-03 23:50:48 +0300

    render cards

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
-
-function App() {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+import PropTypes from 'prop-types';
+function App(props) {
+  const {numbers} = props;
+  return <MainScreen numbers={numbers}/>;
 }
+
+App.propTypes = {
+  numbers: PropTypes.number.isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  NUMBERS: 3,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App numbers={Setting.NUMBERS}/>
   </React.StrictMode>,
   document.getElementById('root'));
