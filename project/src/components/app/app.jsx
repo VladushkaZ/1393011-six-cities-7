741048eb26f80dc4b378a446d4687714b8c3bb4c
Vlada Zorina <zorv@gkomega.ru>
2021-06-15 17:08:15 +0300

    add routs

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import MainScreen from '../main-screen/main-screen';
+import FavoritesCard from '../favorites/favorites';
+import LoginPage from '../login/login';
+import PropertyCard from '../property/property';
+import ErrorPage from '../notfound/notfound';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { AppRoute } from '../../const';
+
 function App(props) {
-  const {numbers} = props;
-  const {cities} = props;
-  const {popular} = props;
-  return <MainScreen numbers={numbers} cities={cities} popular={popular}/>;
+  const { numbers } = props;
+  const { cities } = props;
+  const { popular } = props;
+  return (
+
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <MainScreen numbers={numbers} cities={cities} popular={popular} />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <PropertyCard />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <FavoritesCard />
+        </Route>
+        <Route exact path={AppRoute.LOGIN}>
+          <LoginPage />
+        </Route>
+        <Route>
+          <ErrorPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 App.propTypes = {
